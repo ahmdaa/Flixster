3547292e173f024466867dc82605d34a4233521f
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-02-09 03:04:12 -0500

    Add navigation to a new movie details view controller and pass data to it

--- Flixster/MoviesViewController.swift
+++ Flixster/MoviesViewController.swift
@@ -67,6 +67,21 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         return cell
     }
+    
+    // MARK:- Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        
+        let movie = movies[indexPath.row]
+        
+        // Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
