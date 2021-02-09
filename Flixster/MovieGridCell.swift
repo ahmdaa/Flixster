f5e321258e7bb3753cb063acc3403df10f62b959
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-02-09 18:04:33 -0500

    Add tab bar controller and navigation controller to a new superhero collection view screen displaying movies related to Wonder Woman

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Ahmed Abdalla on 2/9/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
